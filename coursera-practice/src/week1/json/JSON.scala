fe4f82d5d80a70b18029ce523f4862f7ac799743
sibdoma <sibdoma@gmail.com>
2016-09-21 23:50:13 +0300

    init

---
+++ coursera-practice/src/week1/json/JSON.scala
@@ -0,0 +1,15 @@
+package week1.json
+
+abstract class JSON
+
+case class JSeq (elems: List[JSON]) extends JSON
+
+case class JObj (bindings: Map[String, JSON]) extends JSON
+
+case class JNum (num: Double) extends JSON
+
+case class JStr (str: String) extends JSON
+
+case class JBool (b: Boolean) extends JSON
+
+case object JNull extends JSON
---
+++ coursera-practice/src/week1/json/Main.scala
@@ -0,0 +1,36 @@
+package week1.json
+
+object Main extends App {
+  val data = JObj(Map(
+    "firstName" -> JStr("John"),
+    "lastName" -> JStr("Smith"),
+    "address" -> JObj(Map(
+      "streetAddress" -> JStr("21 2nd Street"),
+      "state" -> JStr("NY"),
+      "postalCode" -> JNum(10021)
+    )),
+    "phoneNumbers" -> JSeq(List(
+      JObj(Map(
+        "type" -> JStr("home"), "number" -> JStr("212 555-1234")
+      )),
+      JObj(Map(
+        "type" -> JStr("fax"), "number" -> JStr("646 555-4567")
+      ))))))
+
+  def show(json: JSON): String = json match {
+    case JSeq(elems) =>
+      "[" + (elems map show mkString ", ") + "]"
+    case JObj(bindings) =>
+      val assocs = bindings map {
+        case (key, value) => "\"" + key + "\": " + show(value)
+      }
+      "{" + (assocs mkString ", ") + "}"
+    case JNum(num) => num.toString
+    case JStr(str) => '\"' + str + '\"'
+    case JBool(b) => b.toString
+    case JNull => "null"
+  }
+
+  print(show(data))
+
+}
